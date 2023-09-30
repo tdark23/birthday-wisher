cbf9bc9fb752eafa86b6b127d10be3755f0b27ba
tedmbang <ted.mbang@hintekk.com>
2023-09-30 15:17:35 +0100

    removed the motivational quotes sender program, done with the birthday wisher program

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import smtplib
 import datetime as dt
+import csv
+import os
 import random
 
-# -- Sending quotes on Monday -- #
-
 my_email = "tedmbangudemy@gmail.com"
-app_password = ""  # fill the password for the email here
-ME = "tdark237@gmail.com"
+app_password = "gyqwqlwobzfktjrh"  # fill the password for the email here
 
 
-def send_email(msg):
+def send_email(address, subject, msg):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()  # encryption
         connection.login(user=my_email, password=app_password)
-        connection.sendmail(from_addr=my_email, to_addrs=ME, msg=f"Subject:Monday motivation \n\n{msg}")
+        connection.sendmail(from_addr=my_email, to_addrs=f"{address}", msg=f"Subject:{subject} \n\n{msg}")
 
 
-now = dt.datetime.now()
-weekday = now.weekday()
-if weekday == 1:
-    with open("quotes.txt") as quote_file:
-        # Getting the list of every single line in the file
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+# ----- birthday wisher start here ------- #
 
-    send_email(msg=quote)
+# Getting the date current date object
+today = dt.datetime.now().date()
+
+# Path to the folder containing letter templates
+letter_folder = 'letter_templates'
+# listing the files in the letter templates folder
+letter_files = os.listdir(letter_folder)
+# Select a random letter template
+letter_to_send = random.choice(letter_files)
+
+# Read the CSV file and check for birthdays
+
+with open("birthdays.csv", "r") as birthday_file:
+    reader = csv.DictReader(birthday_file)
+    for row in reader:
+        name = row['name']
+        month_birth = int(row['month'])
+        day_birth = int(row['day'])
+        email = row['email']
+
+        if today.month == month_birth and today.day == day_birth:
+            with open(os.path.join(letter_folder, letter_to_send), "r") as letter:
+                template = letter.read()
+                final_letter = template.replace("[NAME]", name)
+            send_email(address=email, subject="HAPPY BIRTHDAY", msg=final_letter)
